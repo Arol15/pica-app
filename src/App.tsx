98e9c19123b3a1657fac528b856a7c01853eafcd
Lora Rusinouskaya <lrusinouskaya@outlook.com>
2023-06-06 19:59:38 -0400

    add dynamic searchQuery

--- src/App.tsx
+++ src/App.tsx
@@ -9,22 +9,27 @@ interface Image {
 
 function App() {
 
-  const [searchResult, setSearchResult] = useState<Image[]>([])
+  const [searchResult, setSearchResult] = useState<Image[]>([]);
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
 
     const getImages = async () => {
-      const response = await fetch(`${API_URL}&q=flamingo`);
+      const response = await fetch(`${API_URL}&q=${searchQuery}`);
       const data = await response.json();
       setSearchResult(data.hits)
     }
     getImages();
-  }, [])
+  }, [searchQuery]);
 
   return (
     <div>
       <h1>Find your awesome image!</h1>
-      <input placeholder="Search for images "></input>
+      <input placeholder="Search images..." 
+            type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+      />
       <button>Search</button>
       <div>
         {searchResult.length > 0 && searchResult.map(result => (
