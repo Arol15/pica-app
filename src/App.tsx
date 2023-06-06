818196d07f314396b80ac7d8276c6933e48c2e20
Lora Rusinouskaya <lrusinouskaya@outlook.com>
2023-06-06 17:39:37 -0400

    test api url

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+const API_URL = `https://pixabay.com/api/?key=${import.meta.env.VITE_PIXABAY_API_KEY}`
 
 function App() {
 
+  console.log(API_URL)
+
   return (
-    <>
-     Hello pica app
-    </>
+    <div>
+      <h1>Find your awesome image!</h1>
+      <input placeholder="Search for images "></input>
+      <button>Search</button>
+      <div>
+        result images go here...
+      </div>
+    </div>
   )
 }
 
