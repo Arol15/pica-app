ce3be32d550f74640eb2638e08af838f1ec14ee4
Lora Rusinouskaya <lrusinouskaya@outlook.com>
2023-06-07 13:10:13 -0400

    handle search on enter

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -8,12 +8,19 @@ function SearchBar({handleSearch}: SearchBarProps) {
 
   const [inputValue, setInputValue] = useState('');
 
+  function handleKeyDown(event: any) { //todo update type
+    if(event.key === "Enter") {
+      handleSearch(inputValue)
+    }
+  }
+ 
   return (
       <>  
         <input placeholder="Search images..." 
             type="text"
             value={inputValue}
             onChange={(e) => setInputValue(e.target.value)}
+            onKeyDown={handleKeyDown}
          />
          <button onClick={() => handleSearch(inputValue)}>Search</button>
         </>
