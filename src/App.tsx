f6d6a3fd36dece55c6ce569f0f8f5b036168ea73
Lora Rusinouskaya <lrusinouskaya@outlook.com>
2023-06-07 15:42:37 -0400

    apply style for header

--- src/App.tsx
+++ src/App.tsx
@@ -32,13 +32,19 @@ function App() {
   }
 
   return (
-    <div>
-      <h1>Find your awesome image!</h1>
-      <SearchBar handleSearch={handleSearch}/>
-        {status === 'error' && (
+    <div className="wrapper">
+      <header className="header">
+        <div className='logo'> 
+        <span role='img'>📸 </span>
+        <h1>Pica App</h1>
+        </div>
+        <SearchBar handleSearch={handleSearch}/>
+        {searchResult.length > 0 && <p className="num-results">Found {searchResult.length} results</p>}
+      </header>
+      {status === 'error' && (
           <p>Sorry, something went wrong.</p>
         )}
-        {status === 'loading' && <p>Hold tight, your images are loading...</p>}
+        {status === 'loading' && <p>Loading...</p>}
         {searchResult.length === 0 && status === 'success' && <p>No images. Please try another search term.</p>}
       <ResultsDataView searchResult={searchResult}/>
     </div>
